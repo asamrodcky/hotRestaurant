c08c6712b91771c0cf586c862ab120fbc7e1a638
samrod826 <samrodriguez826@gmail.com>
2019-12-11 20:55:07 -0500

    index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendfile(path.join(__dirname + "/../public/indes"));
+  res.sendfile(path.join(__dirname + "/../public/html/index.html"));
+});
+
+router.get('/reservations', function(req, res, next) {
+  res.sendfile(path.join(__dirname + "/../public/html/makeRes.html"));
+});
+
+router.get('/tables', function(req, res, next) {
+  res.sendfile(path.join(__dirname + "/../public/html/tables.html"));
 });
 
 module.exports = router;
