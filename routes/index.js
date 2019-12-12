60c50a95809d991948aa090880178c812876e57c
samrod826 <samrodriguez826@gmail.com>
2019-12-11 20:35:54 -0500

    initial js changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendfile(path.join(__dirname + "/../public/indes"));
 });
 
 module.exports = router;
